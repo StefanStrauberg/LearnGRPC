35670e695e6ea92690cc18fd2c2e563a0fb00835
StefanStrauberg <stefanstrauberg@gmail.com>
2023-12-21 21:47:13 +0300

    Adding protobuf file onto the CLient and register GRPC connection to the Server

--- src/HelloWorld/GrpcHelloWorldClient/Program.cs
+++ src/HelloWorld/GrpcHelloWorldClient/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello World!");
+﻿using Grpc.Net.Client;
+using GrpcHelloWorldClient.Protos;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+var client = new HelloService.HelloServiceClient(channel);
+
+var reply = await client.SayHelloAsync(
+        new HelloRequest { Name = "GRPC Client" }
+    );
+
+Console.WriteLine($"Greetings: {reply.Message}");
