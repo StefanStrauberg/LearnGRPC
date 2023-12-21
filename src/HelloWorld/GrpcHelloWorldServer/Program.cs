69c8666ed4edf54ac9974bae3412b4aa65b153a7
StefanStrauberg <stefanstrauberg@gmail.com>
2023-12-21 20:58:36 +0300

    simplify project

--- src/HelloWorld/GrpcHelloWorldServer/Program.cs
+++ src/HelloWorld/GrpcHelloWorldServer/Program.cs
@@ -1,11 +1,5 @@
-using GrpcHelloWorldServer.Services;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddGrpc();
-builder.Services.AddSingleton<ChatRoom>();
 
 var app = builder.Build();
 
-app.MapGrpcService<HelloWorldService>();
 app.Run();
