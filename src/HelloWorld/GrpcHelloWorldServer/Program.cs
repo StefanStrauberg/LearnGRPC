3e7c4dabc64aae27133b613da3b32d37eb0cebd4
StefanStrauberg <stefanstrauberg@gmail.com>
2023-12-21 21:37:28 +0300

    Adding protobuf file, service, and register grpc into Server

--- src/HelloWorld/GrpcHelloWorldServer/Program.cs
+++ src/HelloWorld/GrpcHelloWorldServer/Program.cs
@@ -1,5 +1,9 @@
+using GrpcHelloWorldServer.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddGrpc();
 
 var app = builder.Build();
 
+app.MapGrpcService<HelloWorldService>();
 app.Run();
