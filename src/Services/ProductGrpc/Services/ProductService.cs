e3861eabe0c9ef50ad03c8c883ea920832334652
StefanStrauberg <stefanstrauberg@gmail.com>
2023-12-23 13:36:24 +0300

    Update ProductService and ProductGerpcClient for get an one product and get all products

--- src/Services/ProductGrpc/Services/ProductService.cs
+++ src/Services/ProductGrpc/Services/ProductService.cs
@@ -1,11 +1,13 @@
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 using ProductGrpc.Data;
 using ProductGrpc.Protos;
 
 namespace ProductGrpc.Services;
 
-public class ProductService(ILogger<ProductService> logger, ProductContext context) : ProductProtoService.ProductProtoServiceBase
+public class ProductService(ILogger<ProductService> logger,
+                            ProductContext context) : ProductProtoService.ProductProtoServiceBase
 {
     readonly ILogger<ProductService> _logger = logger 
         ?? throw new ArgumentNullException(nameof(logger));
@@ -17,7 +19,8 @@ public class ProductService(ILogger<ProductService> logger, ProductContext conte
         return base.Test(request, context);
     }
 
-    public override async Task<ProductModel> GetProduct(GetProductRequest request, ServerCallContext context)
+    public override async Task<ProductModel> GetProduct(GetProductRequest request,
+                                                        ServerCallContext context)
     {
         var product = await _context.Products.FindAsync(request.ProductId);
         
@@ -37,5 +40,27 @@ public class ProductService(ILogger<ProductService> logger, ProductContext conte
         };
 
         return productModel;
+    }
+
+    public override async Task GetAllProducts(GetAllProductsRequest request,
+                                              IServerStreamWriter<ProductModel> responseStream,
+                                              ServerCallContext context)
+    {
+        var products = await _context.Products.ToListAsync();
+
+        foreach (var item in products)
+        {
+            var productModel = new ProductModel
+            {
+                ProductId = item.ProductId,
+                Name = item.Name,
+                Description = item.Description,
+                Price = item.Price,
+                Status = ProductStatus.Instock,
+                CreatedTime = Timestamp.FromDateTime(item.CreatedTime)
+            };
+
+            await responseStream.WriteAsync(productModel);
+        }
     }
 }
--- src/Services/ProductGrpcClient/Program.cs
+++ src/Services/ProductGrpcClient/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using ProductGrpcClient.Protos;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5005");
+var client = new ProductProtoService.ProductProtoServiceClient(channel);
+
+// GetProductAsync
+Console.WriteLine(new string('-',50));
+Console.WriteLine("GetProductAsync started..");
+var getProductRequest = new GetProductRequest { ProductId = 1 }; 
+var response = await client.GetProductAsync(getProductRequest);
+Console.WriteLine(response);
+
+// GetAllProductsAsync
+Console.WriteLine(new string('-',50));
+Console.WriteLine("GetAllProductsAsync started..");
+var getAllProductsRequest = new GetAllProductsRequest();
+using (var clientData = client.GetAllProducts(getAllProductsRequest))
+{
+    await foreach (var currentProduct in clientData.ResponseStream.ReadAllAsync())
+        Console.WriteLine(currentProduct);
+}
+
+// AddProductAsync
+Console.WriteLine(new string('-',50));
+Console.WriteLine("AddProductAsync started..");
