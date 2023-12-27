ea154d138b5d63843aad4d4fa80f40fc277f6c54
StefanStrauberg <stefanstrauberg@gmail.com>
2023-12-27 20:58:25 +0300

    update Client - put actions in separate methods

--- src/Services/ProductGrpcClient/Program.cs
+++ src/Services/ProductGrpcClient/Program.cs
@@ -1,27 +1,35 @@
 ﻿using Grpc.Core;
 using Grpc.Net.Client;
 using ProductGrpcClient.Protos;
+using static ProductGrpcClient.Protos.ProductProtoService;
 
 using var channel = GrpcChannel.ForAddress("http://localhost:5005");
-var client = new ProductProtoService.ProductProtoServiceClient(channel);
+var client = new ProductProtoServiceClient(channel);
 
 // GetProductAsync
-Console.WriteLine(new string('-',50));
-Console.WriteLine("GetProductAsync started..");
-var getProductRequest = new GetProductRequest { ProductId = 1 }; 
-var response = await client.GetProductAsync(getProductRequest);
-Console.WriteLine(response);
-
+await GetProductAsync(client);
 // GetAllProductsAsync
-Console.WriteLine(new string('-',50));
-Console.WriteLine("GetAllProductsAsync started..");
-var getAllProductsRequest = new GetAllProductsRequest();
-using (var clientData = client.GetAllProducts(getAllProductsRequest))
-{
-    await foreach (var currentProduct in clientData.ResponseStream.ReadAllAsync())
-        Console.WriteLine(currentProduct);
-}
+await GetAllProductsAsync(client);
 
 // AddProductAsync
 Console.WriteLine(new string('-',50));
 Console.WriteLine("AddProductAsync started..");
+
+static async Task GetProductAsync(ProductProtoServiceClient client)
+{
+    Console.WriteLine(new string('-',50));
+    Console.WriteLine("GetProductAsync started..");
+    var getProductRequest = new GetProductRequest { ProductId = 1 }; 
+    var response = await client.GetProductAsync(getProductRequest);
+    Console.WriteLine(response);
+}
+
+static async Task GetAllProductsAsync(ProductProtoServiceClient client)
+{
+    Console.WriteLine(new string('-',50));
+    Console.WriteLine("GetAllProductsAsync started..");
+    var getAllProductsRequest = new GetAllProductsRequest();
+    using var clientData = client.GetAllProducts(getAllProductsRequest);
+    await foreach (var currentProduct in clientData.ResponseStream.ReadAllAsync())
+        Console.WriteLine(currentProduct);
+}
